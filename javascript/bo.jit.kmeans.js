7608aa0ca6518af9eb704d34574f840eb44c8795
Vincent Goudard <vincentgoudard@gmail.com>
2026-03-29 23:46:44 +0200

    Create bo.jit.kmeans.js

---
+++ javascript/bo.jit.kmeans.js
@@ -0,0 +1,170 @@
+inlets = 1;
+outlets = 2; // outlet 0 = labels, outlet 1 = sorted jit.matrix
+
+// Parameters
+var K = 3;
+var maxIter = 100;
+
+// --- Entry point ---
+function jit_matrix(name) {
+    var m = new JitterMatrix(name);
+
+    var N = m.dim[0]; // number of series
+    var M = m.dim[1]; // samples per series
+
+    // --- Extract data correctly ---
+    var data = [];
+    for (var i = 0; i < N; i++) {
+        data[i] = [];
+        for (var j = 0; j < M; j++) {
+            data[i][j] = m.getcell(i, j)[0];
+        }
+    }
+
+    // --- Build full correlation feature space ---
+    var points = [];
+    for (var i = 0; i < N; i++) {
+        points[i] = [];
+        for (var j = 0; j < N; j++) {
+            points[i][j] = correlation(data[i], data[j]);
+        }
+    }
+
+    // --- Run K-means ---
+    var labels = kmeans(points, K, maxIter);
+
+    // --- Convert labels to start at 1 ---
+    for (var i = 0; i < labels.length; i++) labels[i] += 1;
+
+    // --- Output labels ---
+    outlet(0, labels);
+
+    // --- Reorder matrix rows according to cluster index ---
+    var sortedIndices = [];
+    for (var c = 1; c <= K; c++) {
+        for (var i = 0; i < labels.length; i++) {
+            if (labels[i] === c) {
+                sortedIndices.push(i);
+            }
+        }
+    }
+
+//    // --- Create a new JitterMatrix for output ---
+//    var sortedMatrix = new JitterMatrix();
+//    sortedMatrix.type = m.type;
+//    sortedMatrix.dim = [N, M];
+//    sortedMatrix.planecount = 1;
+//
+//    for (var i = 0; i < N; i++) {
+//        var srcIdx = sortedIndices[i];
+//        for (var j = 0; j < M; j++) {
+//            sortedMatrix.setcell2d(i, j, data[srcIdx][j]);
+//        }
+//    }
+//
+//    // --- Output the sorted matrix ---
+//    outlet(1, "jit_matrix", sortedMatrix.name);
+}
+
+// --- Correlation ---
+function correlation(a, b) {
+    var n = a.length;
+    var meanA = mean(a);
+    var meanB = mean(b);
+
+    var num = 0;
+    var denA = 0;
+    var denB = 0;
+
+    for (var i = 0; i < n; i++) {
+        var da = a[i] - meanA;
+        var db = b[i] - meanB;
+        num += da * db;
+        denA += da * da;
+        denB += db * db;
+    }
+
+    var denom = Math.sqrt(denA * denB);
+    if (denom < 1e-12) return 0;
+    return num / denom;
+}
+
+function mean(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) sum += arr[i];
+    return sum / arr.length;
+}
+
+// --- Deterministic K-means ---
+function kmeans(points, k, maxIter) {
+    var N = points.length;
+    var dim = points[0].length;
+
+    // --- Deterministic initialization: first K points ---
+    var centroids = [];
+    for (var i = 0; i < k; i++) {
+        centroids[i] = points[i].slice();
+    }
+
+    var labels = new Array(N);
+
+    for (var iter = 0; iter < maxIter; iter++) {
+        // Assign step
+        for (var i = 0; i < N; i++) {
+            var minDist = Infinity;
+            var best = 0;
+            for (var c = 0; c < k; c++) {
+                var d = distance(points[i], centroids[c]);
+                if (d < minDist) {
+                    minDist = d;
+                    best = c;
+                }
+            }
+            labels[i] = best;
+        }
+
+        // Update step
+        var counts = new Array(k).fill(0);
+        var newCentroids = [];
+        for (var c = 0; c < k; c++) newCentroids[c] = new Array(dim).fill(0);
+
+        for (var i = 0; i < N; i++) {
+            var label = labels[i];
+            counts[label]++;
+            for (var d = 0; d < dim; d++) newCentroids[label][d] += points[i][d];
+        }
+
+        for (var c = 0; c < k; c++) {
+            if (counts[c] > 0) {
+                for (var d = 0; d < dim; d++) newCentroids[c][d] /= counts[c];
+            } else {
+                // deterministic: pick the first point not used as a centroid yet
+                for (var i = 0; i < N; i++) {
+                    if (!centroids.some(x => x === points[i])) {
+                        newCentroids[c] = points[i].slice();
+                        break;
+                    }
+                }
+            }
+        }
+        centroids = newCentroids;
+    }
+
+    return labels;
+}
+
+// --- Distance ---
+function distance(a, b) {
+    var sum = 0;
+    for (var i = 0; i < a.length; i++) {
+        var d = a[i] - b[i];
+        if (isNaN(d)) return Infinity;
+        sum += d * d;
+    }
+    return Math.sqrt(sum);
+}
+
+// --- Set K from Max ---
+function ncluster(v) {
+    K = Math.max(1, v);
+}
